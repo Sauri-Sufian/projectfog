78c91cbed129b9eeeffc47319b5454518547656f
saurisufian <abusauri.iub@gmail.com>
2020-07-25 23:10:46 +0600

    Updates

---
+++ precache-manifest.1377c042def9222bf9bbf541d34c0105.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6025f33d5c5bd2049c8c7dc50e54d3e6",
+    "url": "/projectfog/index.html"
+  },
+  {
+    "revision": "32116fa42354a279a979",
+    "url": "/projectfog/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8f7bf379d64685cd366d",
+    "url": "/projectfog/static/css/main.382c286c.chunk.css"
+  },
+  {
+    "revision": "32116fa42354a279a979",
+    "url": "/projectfog/static/js/2.0ae2288a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/projectfog/static/js/2.0ae2288a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7bf379d64685cd366d",
+    "url": "/projectfog/static/js/main.3d446a4c.chunk.js"
+  },
+  {
+    "revision": "ae8d31f9c92865fadac9",
+    "url": "/projectfog/static/js/runtime-main.e25e1934.js"
+  },
+  {
+    "revision": "7fe11d7003b7fa9d56ebdc20a7006b89",
+    "url": "/projectfog/static/media/im8Logo.7fe11d70.png"
+  },
+  {
+    "revision": "5933d0baeec0dfc578997e2f0eba3f0b",
+    "url": "/projectfog/static/media/user.5933d0ba.png"
+  }
+]);
